0bdbe70f5252fdaa5bb91c27a0771a760b08d277
scumatteo <matteo.scucchia@studio.unibo.it>
2024-12-03 18:58:44 +0100

    removed rtabmap

--- rtabmap/corelib/src/region/losses/CrossEntropyLoss.cpp
+++
@@ -1,9 +0,0 @@
-#include "rtabmap/core/region/losses/CrossEntropyLoss.h"
-
-namespace rtabmap
-{
-    torch::Tensor CrossEntropyLossImpl::compute(const torch::Tensor &input, const torch::Tensor &target)
-    {
-        return this->forward(input, target);
-    }
-}
